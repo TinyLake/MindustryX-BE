9d651ca03ed58747ffbd0fcd2c3c99de07808e27
way-zer <himc.wicp@gmail.com>
2022-03-02 18:12:18 +0800

    API: add SendPacketEvent

--- core/src/mindustry/net/ArcNetProvider.java
+++ core/src/mindustry/net/ArcNetProvider.java
@@ -13,6 +13,7 @@ import arc.util.io.*;
 import mindustry.net.Administration.*;
 import mindustry.net.Net.*;
 import mindustry.net.Packets.*;
+import mindustryX.events.*;
 import net.jpountz.lz4.*;
 
 import java.io.*;
@@ -303,6 +304,7 @@ public class ArcNetProvider implements NetProvider{
 
         @Override
         public void sendStream(Streamable stream){
+            if(!SendPacketEvent.emit(this, null, stream)) return;
             connection.addListener(new InputStreamSender(stream.stream, 512){
                 int id;
 
@@ -328,6 +330,7 @@ public class ArcNetProvider implements NetProvider{
 
         @Override
         public void send(Object object, boolean reliable){
+            if(!SendPacketEvent.emit(this, null, object)) return;
             try{
                 if(reliable){
                     connection.sendTCP(object);
